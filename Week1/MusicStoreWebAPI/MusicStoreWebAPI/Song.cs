be5193933520bf0bfbb137026a81e78960a3af84
Yasin Anil Bostanci <44286549+yasinanilboss@users.noreply.github.com>
2023-02-01 17:20:12 +0300

    Add files via upload

---
+++ Week1/MusicStoreWebAPI/MusicStoreWebAPI/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStoreWebAPI
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        [Required]
+        public string Duration { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+
+
+
+    }
+}
